87b737ce8f5a632c158446632c0501782d78d84b
adem-genc53 <tutastes@gmail.com>
2024-07-01 22:34:45 +0300

    Güncellendi

--- includes/webyonetimi.sql
+++ includes/webyonetimi.sql
@@ -88,7 +88,7 @@ CREATE TABLE IF NOT EXISTS `user_logins` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `login_time` bigint NOT NULL,
-  `log_in_from` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `log_in_from` varchar(16) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -109,8 +109,8 @@ CREATE TABLE IF NOT EXISTS `uyeler` (
   `user_password_hash` char(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `user_email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `user_group` int NOT NULL,
-  `remember_me_token` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `token_expiry` datetime DEFAULT NULL,
+  `remember_me_token` varchar(64) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `token_expiry` bigint DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name` (`user_name`),
   UNIQUE KEY `email_user` (`user_email`)
