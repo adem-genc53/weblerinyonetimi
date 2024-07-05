0f30890c2256b4c873963b4fa1793cb56499507d
adem-genc53 <tutastes@gmail.com>
2024-07-05 16:33:06 +0300

    Güncellendi

--- register.php
+++ register.php
@@ -2,13 +2,4 @@
 // Bismillahirrahmanirrahim
 require_once __DIR__ . '/includes/connect.php';
 
-// Oturum kimliğini yenileme süresini belirleyin (örneğin 15 dakika)
-$regenerate_time = 15 * 60; // 15 dakika
-// Şu anki zaman ve oturum başlangıç zamanını kontrol edin
-if (isset($_SESSION['start_time']) && (time() - $_SESSION['start_time'] > $regenerate_time) && isset($_SESSION)) {
-    // Oturum kimliğini yenileyin
-    session_regenerate_id(true);
-    // Yeni başlangıç zamanını ayarlayın
-    $_SESSION['start_time'] = time();
-}
 ?>
