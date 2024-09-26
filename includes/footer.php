f3882cb77d591575750504db7cf4f6356786956c
Adem GENÇ <159489452+adem-genc53@users.noreply.github.com>
2024-09-26 12:02:06 +0300

    Güncellendi

--- includes/footer.php
+++ includes/footer.php
@@ -2,13 +2,7 @@
 
         <!-- Main Footer -->
         <footer class="main-footer" style="border-top: none;">
-            <form method="POST" id="mobil_pc">
-                <input type="hidden" name="mobil_pc">
-                    <b>&copy; 2002-<?php echo date("Y"); ?> Tüm Haklar <a target="_blank" href="https://uzaysat.com.tr">UZAYSAT BİNA ELEKTRONİK SİS SAN VE TİC LTD ŞTİ</a>.</b> aittir & kopyalanamaz.
-                    <div class="float-right d-none- d-sm-inline-block"><span id="mobil-versiyon">
-                        Mobil Versiyon: <input type="checkbox" name="gecis" value="2" <?php if(isset($_COOKIE['mobil_pc_gecis']) && $_COOKIE['mobil_pc_gecis']=='mobil'){ echo "checked"; } ?> onchange="document.getElementById('mobil_pc').submit()"></span> <b>Version</b> <?php echo VERSIYON; ?>
-                    </div>
-            </form>
+            <b>&copy; 2002-<?php echo date("Y"); ?> Tüm Haklar <a target="_blank" href="https://websiteler.yonetimi.com">WEB SİTELER YÖNETİMİ</a>.</b> aittir & kopyalanamaz.
         </footer>
         </div><!-- jswindow_website_cerceve -->
     </div><!-- ./wrapper -->
