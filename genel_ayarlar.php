ac3d641ddc7f72dd286599890bff4e30e37a4ead
Adem GENÇ <159489452+adem-genc53@users.noreply.github.com>
2024-09-28 10:58:43 +0300

    Güncellendi

--- genel_ayarlar.php
+++ genel_ayarlar.php
@@ -902,9 +902,9 @@ $(document).ready(function() {
                                                             <td style="padding: 0rem 0.75rem 0rem 0.75rem;vertical-align: middle;min-width: 200px;">
                                                             <?php 
                                                             if(isset($genel_ayarlar['ftp_mode']) && $genel_ayarlar['ftp_mode']==0){
-                                                                echo "<input type='radio' name='ftp_mode' value='1' checked>";
+                                                                echo "<input type='radio' name='ftp_mode' value='0' checked>";
                                                             } else if(isset($genel_ayarlar['ftp_mode']) && $genel_ayarlar['ftp_mode']==1){
-                                                                echo "<input type='radio' name='ftp_mode' value='1'>";
+                                                                echo "<input type='radio' name='ftp_mode' value='0'>";
                                                             }
                                                             ?>
                                                             </td>
